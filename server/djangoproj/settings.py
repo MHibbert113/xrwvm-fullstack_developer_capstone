2a6de11cf90cae7c057cd6621668648a80726825
MHibbert113 <MHibbert113@outlook.com>
2025-10-08 10:05:30 -0400

    Save Progress

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -144,5 +144,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
-ALLOWED_HOSTS=['localhost','https://mhibbert113-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://mhibbert113-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=['localhost','https://mhibbert113-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://mhibbert113-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
